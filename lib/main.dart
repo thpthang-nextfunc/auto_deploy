e89f3409ba3578548dda889f86a8c47391674b00
Thang Tran <69334860+thpthang-nextfunc@users.noreply.github.com>
2024-01-30 14:57:24 +0700

    change text

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Update test auto deploy 1'),
+      home: const MyHomePage(title: 'Update test auto deploy 1 again'),
     );
   }
 }
